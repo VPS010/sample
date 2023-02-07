e93f39798239c851856d7a711079bc4fc43c1064
Vinay <vinayprakashsenapati01@gmail.com>
2023-02-07 21:18:24 +0530

    sorted in code 3

--- code3.js
+++ code3.js
@@ -1,4 +1,4 @@
-let a =['sd','vinay',12,"dvv",2,32]
+let a =['sd','vinay',12,"d",2,32]
 for(let x of a)
 {
     console.log(a)
@@ -8,10 +8,11 @@ for(let b in a)
     console.log(a[b])
 }
 console.log('  ')
-let b = [22,4,22,42]
+let b = [22,4,2,42]
 let c = b.length
 console.log(c)
 while(c){
     console.log(b[c-1])
     c--
 }
+console.log(b.sort())
